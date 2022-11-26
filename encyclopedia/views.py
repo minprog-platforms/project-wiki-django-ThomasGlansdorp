702984dc2e7bc36447b593f1733b20cd184078b8
ThomasGlansdorp <thomas.glansdorp@gmail.com>
2022-11-26 12:48:08 +0100

    Entry page and index page added to the page list.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from . import util
@@ -6,5 +7,14 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    entry_content = util.get_entry(title)
+    if entry_content is None:
+        raise Http404("requested page not found")
+    return render(request, "encyclopedia/entry.html", {
+        "entry": util.get_entry(title),
+        "entry_title": title
     })
 
