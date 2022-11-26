64af1f811b6d636e757a545d8629278db776bd75
ThomasGlansdorp <thomas.glansdorp@gmail.com>
2022-11-26 13:20:03 +0100

    Index function changed to allow for searching of encyclopedia pages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,15 +1,24 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from . import util
 
 
 def index(request):
+    all_entries = util.list_entries()
+    query = request.GET.get("q", "")
+    if query is not "" and query in all_entries:
+        return redirect(f"wiki/{query}")
+    entries = []
+    for entry in all_entries:
+        if query in entry:
+            entries.append(entry)
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries
     })
 
-def entry(request, title):
+
+def wiki_entry(request, title):
     entry_content = util.get_entry(title)
     if entry_content is None:
         raise Http404("requested page not found")
